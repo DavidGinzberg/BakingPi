9cf34e475f9c8ad7314cde92f18fc0ce9bef3d21
David Ginzberg <arguing.with.my.keyboard@gmail.com>
2015-07-02 11:05:34 -0400

    Change comments to use semicolon-strudel. Prevents bad instruction errors

--- OK01/source/main.s
+++ OK01/source/main.s
@@ -2,13 +2,13 @@
 .globl _start
 _start:
 
-ldr r0,=0x20200000  ;GPIO controller address
+ldr r0,=0x20200000  ;@GPIO controller address
 mov r1,#1
-lsl r1,#18          ;Pin 16 is sixth bit-triplet in the GPIO 10-19 block -- 6x3=18
-str r1,[r0,#4]      ;Load r1 into GPIO controller DWORD 2
+lsl r1,#18          ;@Pin 16 is sixth bit-triplet in the GPIO 10-19 block -- 6x3=18
+str r1,[r0,#4]      ;@Load r1 into GPIO controller DWORD 2
 mov r1,#1
 lsl r1,#16          
-str r1,[r0,#40]     ;GPIO Pin Off address (GPIO+28 is on)
+str r1,[r0,#40]     ;@GPIO Pin Off address (GPIO+28 is on)
 
 loop$:
 
