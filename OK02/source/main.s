c5a39b0193cd668f62c51203853879633f2f4d61
David Ginzberg <arguing.with.my.keyboard@gmail.com>
2015-07-03 12:31:48 -0400

    Rewrote OK02 source code to switch ACT led on and off

--- OK02/source/main.s
+++ OK02/source/main.s
@@ -8,8 +8,23 @@ lsl r1,#18          ;@Pin 16 is sixth bit-triplet in the GPIO 10-19 block -- 6x3
 str r1,[r0,#4]      ;@Load r1 into GPIO controller DWORD 2
 mov r1,#1
 lsl r1,#16          
+
+loop$:		    ;@Loop on turning the ACT led on and off
 str r1,[r0,#40]     ;@GPIO Pin Off address (GPIO+28 is on)
 
-loop$:
+mov r2, #0x3F0000   ;@Wait before next step
+wait1$:
+sub r2, #1
+cmp r2, #0
+bne wait1$
+
+str r1,[r0,#28]     ;@GPIO Pin on address
+
+mov r2, #0x3F0000
+wait2$:
+sub r2, #1
+cmp r2, #0
+bne wait2$
+
 
 b loop$
